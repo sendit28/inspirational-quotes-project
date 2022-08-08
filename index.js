f93d7bc89417253d46ee0be7d8cdff84bac779ce
+ <+>
2022-08-07 20:10:23 -0400

    added header paragraph button event handler for button click

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-console.log("hello world")
+// console.log("hello world")
 
-fetch("https://api.quotable.io/random?tags=inspirational")
-    .then((response) => response.json())
-    .then((data) => console.log(data))
-    .catch(error => console.log(error)) 
+// fetch("https://api.quotable.io/random?tags=inspirational")
+//     .then((response) => response.json())
+//     .then((data) => console.log(data))
+//     .catch(error => console.log(error)) 
+
+const button = document.getElementById("btn-new-quote")
+console.log(button)
+button.addEventListener("click", (event) =>{
+    event.preventDefault()
+    console.log("clicked")
+    const quote = document.getElementById("quote")
+    console.log(quote)
+    quote.innerHTML = "X"
+})
