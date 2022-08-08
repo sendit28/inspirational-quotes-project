a8db81e916640db947c6af9ce71787d24280ebaa
+ <+>
2022-08-08 18:49:37 -0400

    changes to getNewQuote to the .then format

--- index.js
+++ index.js
@@ -9,23 +9,35 @@ const button = document.getElementById("btn-new-quote")
 const quote = document.getElementById("quote")
 // console.log(button)
 button.addEventListener("click", (event) =>{
-    event.preventDefault()
     // console.log("clicked")
-    
     // console.log(quote)
     getNextQuote()
 })
 
-async function getNextQuote() {
-    const response = await fetch("https://api.quotable.io/random?tags=inspirational")
-    const data = await response.json();
+// async function getNextQuote() {
+//     const response = await fetch("https://api.quotable.io/random?tags=inspirational")
+//     const data = await response.json()
     
-        console.log(data)
-        if (response.ok) {
+//         console.log(data)
+//         if (response.ok) {
+//             quote.innerHTML = data.content
+//         } else {
+//             quote.innerHTML = "An error occurred"
+//             console.log(data)
+//         }        
+// }
+
+function getNextQuote() {
+    fetch("https://api.quotable.io/random?tags=inspirational")
+        .then((response) => response.json())
+        .then((data) => {
             quote.innerHTML = data.content
-        } else {
-            quote.innerHTML = "An error occurred"
-            console.log(data)
-        }        
+        })
+        .catch(error => console.log(error))
 }
 
+
+fetch("https://imsea.herokuapp.com/api/1?q=sunset",{mode: 'cors'})
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch(error => console.log(error)) 
