46728e5b75f7a4344effb4f8369511e93400fd49
+ <+>
2022-08-16 10:56:59 -0400

    removes comments

--- index.js
+++ index.js
@@ -1,16 +1,7 @@
-// console.log("hello world")
-// https://github.com/lukePeavey/quotable
-// fetch("https://api.quotable.io/random?tags=inspirational")
-//     .then((response) => response.json())
-//     .then((data) => console.log(data))
-//     .catch(error => console.log(error)) 
-
 const button = document.getElementById("btn-new-quote")
 const quote = document.getElementById("quote")
-// console.log(button)
+
 button.addEventListener("click", (event) =>{
-    // console.log("clicked")
-    // console.log(quote)
     getNextQuote()
 })
 
@@ -23,35 +14,28 @@ function getNextQuote() {
         .catch(error => console.log(error))
 }
 
-
 const form = document.getElementById("gratitude-form")
 form.addEventListener("submit", (event) => {
     event.preventDefault()
-    const container = document.getElementById("gratitude_container")
+    const container = document.getElementById("gratitude-container")
     container.innerHTML = ""
-    // console.log(event.target)
-    // // after submit, I want the text to display on the DOM
-    // How do I get the text from this form
-    // console.log(event.target.children[1])
-   const gratitude1 = event.target.messageOne.value
-   const p1 = document.createElement("p")
-   p1.innerText = gratitude1
-   
-   container.append(p1)  //this is where text appears on DOM
-   //Why is it happening multiple times?
-   
-   const gratitude2 = event.target.messageTwo.value
-   const p2 = document.createElement("p")
-   p2.innerText = gratitude2
-   container.append(p2)  //this is where text appears on DOM
+    
+    const gratitude1 = event.target.messageOne.value
+    const p1 = document.createElement("p")
+    p1.innerText = gratitude1
+    container.append(p1)  
 
-   const gratitude3 = event.target.messageThree.value
-   const p3 = document.createElement("p")
-   p3.innerText = gratitude3
+    const gratitude2 = event.target.messageTwo.value
+    const p2 = document.createElement("p")
+    p2.innerText = gratitude2
+    container.append(p2)  
    
-   container.append(p3)  //this is where text appears on DOM
-   form.reset()
-   
+    const gratitude3 = event.target.messageThree.value
+    const p3 = document.createElement("p")
+    p3.innerText = gratitude3
+    container.append(p3)  
+    
+    form.reset()   
 })   
 
 
@@ -59,7 +43,3 @@ form.addEventListener("submit", (event) => {
 
 
 
-// fetch("https://imsea.herokuapp.com/api/1?q=sunset",{mode: 'cors'})
-//     .then((response) => response.json())
-//     .then((data) => console.log(data))
-//     .catch(error => console.log(error)) 
