6829d5508be3d7b817fe16f45e92e1f5b9957a77
+ <+>
2022-08-16 10:57:55 -0400

    organizes comment notes

--- comment.js
+++ comment.js
@@ -1,3 +1,12 @@
+// console.log("hello world")
+
+// https://github.com/lukePeavey/quotable
+
+// fetch("https://api.quotable.io/random?tags=inspirational")
+//     .then((response) => response.json())
+//     .then((data) => console.log(data))
+//     .catch(error => console.log(error)) 
+
 // async function getNextQuote() {
     //     const response = await fetch("https://api.quotable.io/random?tags=inspirational")
     //     const data = await response.json()
